b5f8a5d4af4e500f65139d768ed6505ef2a4af19
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-27 18:45:00 +0000

    Test for functionality

--- src/main/java/com/bexos/aop/AopApplication.java
+++ src/main/java/com/bexos/aop/AopApplication.java
@@ -1,13 +1,22 @@
 package com.bexos.aop;
 
+import com.bexos.aop.model.User;
+import com.bexos.aop.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class AopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AopApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AopApplication.class, args);
+
+		UserService userService = context.getBean(UserService.class);
+
+		userService.addUser(User.builder().age(20).name("user").build());
 	}
 
 }
