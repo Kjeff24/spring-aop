00a35df4b98309199ce26a957c610cbd09d6095c
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-28 20:39:25 +0000

    Added after advice and around advice

--- src/main/java/com/bexos/aop/aspect/LoggerAspect.java
+++ src/main/java/com/bexos/aop/aspect/LoggerAspect.java
@@ -1,15 +1,35 @@
 package com.bexos.aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggerAspect {
 
+    @Pointcut("execution(* com.bexos.aop.service.*.*(..))")
+    public void serviceMethods() {}
+
     @Before("execution(* com.bexos.aop.service.UserService.addUser(..))")
-    public void logs() {
+    public void beforeAdvice() {
         System.out.println("AOP BEFORE");
+    }
+
+    @After("serviceMethods()")
+    public void afterAdvice() {
+        System.out.println("[AFTER] Method execution completed");
+    }
+
+    @Around("serviceMethods()")
+    public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("[AROUND] Before executing: " + joinPoint.getSignature());
+        Object result = joinPoint.proceed();
+        System.out.println("[AROUND] After executing: " + joinPoint.getSignature());
+        return result;
     }
 }
