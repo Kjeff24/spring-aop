ed636a51b79c062cb7425f0acd0d51d2f6ae80dd
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-28 20:46:18 +0000

    Test after and around advice

--- src/main/java/com/bexos/aop/AopApplication.java
+++ src/main/java/com/bexos/aop/AopApplication.java
@@ -2,21 +2,25 @@ package com.bexos.aop;
 
 import com.bexos.aop.model.User;
 import com.bexos.aop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class AopApplication {
+@RequiredArgsConstructor
+public class AopApplication implements CommandLineRunner {
+    private final UserService userService;
 
-	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(AopApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
 
-		UserService userService = context.getBean(UserService.class);
-
-		userService.addUser(User.builder().age(20).name("user").build());
-	}
-
+    @Override
+    public void run(String... args) throws Exception {
+        userService.addUser(User.builder().age(20).name("user").build());
+        userService.getAllUsers();
+    }
 }
